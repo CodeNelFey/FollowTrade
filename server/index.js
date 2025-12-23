0b332dadc7300bf7a04f2f204a8bf46bfcd6be9f
CodeNelFey <nelfey.desing@gmail.com>
2025-12-23 23:15:18 +0100

    fix: photo

--- server/index.js
+++ server/index.js
@@ -13,7 +13,10 @@ const PORT = 3000;
 const SECRET_KEY = process.env.SECRET_KEY || "super_secret_cle";
 
 app.use(cors());
-app.use(express.json());
+
+// --- CORRECTION : Augmenter la limite pour les images ---
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // --- MULTER (Upload Images) ---
 const uploadDir = path.join(__dirname, 'uploads');
